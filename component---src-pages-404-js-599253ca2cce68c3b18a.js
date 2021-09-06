61249037dd459d25ba89b29d63ba1d2b2eded44b
YuanTian1991 <tianyuan1991hit@gmail.com>
2021-09-06 20:50:07 +0100

    Updates

---
+++ component---src-pages-404-js-599253ca2cce68c3b18a.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[883],{9616:function(e,t,a){a.r(t);var n=a(7294),u=a(3574),l=a(3751);t.default=function(){return n.createElement(u.Z,null,n.createElement(l.Z,{title:"404: Not found"}),n.createElement("h1",null,"NOT FOUND"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-599253ca2cce68c3b18a.js.map
